2e7e7f0509d734df9169018549ace2dc40a0e4b7
unknown <23552011434@utb-univ.ac.id>
2026-09-16 13:16:46 +0700

    fix: switch to neon http driver to resolve vercel timeouts

--- apps/api/src/lib/db.ts
+++ apps/api/src/lib/db.ts
@@ -1,13 +1,7 @@
-import postgres from 'postgres';
+import { neon } from '@neondatabase/serverless';
 
 const dbUrl = 'postgresql://digital_inspect_owner:EaC3nQ5LkxBf@ep-curly-recipe-a1m4d6s2.ap-southeast-1.aws.neon.tech/digital_inspect?sslmode=require';
 
-// Set up the postgres client
-const sql = postgres(dbUrl, {
-  max: 10,
-  idle_timeout: 20,
-  connect_timeout: 10,
-  ssl: 'require'
-});
+const sql = neon(dbUrl);
 
 export default sql;
