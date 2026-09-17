78e5bfa81339e15c329899516733fc80792d27f4
unknown <23552011434@utb-univ.ac.id>
2026-09-17 13:40:31 +0700

    fix(api): mock postgres.js begin and json methods for pg wrapper to fix 500 error on transactions

--- apps/api/src/lib/db.ts
+++ apps/api/src/lib/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const dbUrl = process.env.DATABASE_URL;
 if (!dbUrl) {
-  throw new Error("DATABASE_URL environment variable is missing!");
+  throw new Error('DATABASE_URL environment variable is missing!');
 }
 
 const pool = new Pool({
@@ -13,11 +13,38 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-export default async function sql(strings: TemplateStringsArray, ...values: any[]) {
+const sql = async function(strings: TemplateStringsArray, ...values: any[]) {
   let query = strings[0];
   for (let i = 1; i < strings.length; i++) {
-    query += `$${i}` + strings[i];
+    query += '$' + i + strings[i];
   }
   const result = await pool.query(query, values);
   return result.rows;
-}
+} as any;
+
+sql.json = (obj: any) => JSON.stringify(obj);
+
+sql.begin = async (callback: any) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const tx = async (strings: TemplateStringsArray, ...values: any[]) => {
+      let query = strings[0];
+      for (let i = 1; i < strings.length; i++) {
+        query += '$' + i + strings[i];
+      }
+      const result = await client.query(query, values);
+      return result.rows;
+    };
+    const result = await callback(tx);
+    await client.query('COMMIT');
+    return result;
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
+};
+
+export default sql;
